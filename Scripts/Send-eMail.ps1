bcd966faa788ab85980e12a555fd57e262a5a076
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-08-31 22:42:02 -0400

    Create Send-eMail.ps1

---
+++ Scripts/Send-eMail.ps1
@@ -0,0 +1,120 @@
+﻿#requires -Version 3.0 -Modules Microsoft.PowerShell.Utility
+
+
+
+#region Portable_Section 
+
+# The "$SplatSendEmail" should be the only information you need to change to send an email.  
+$SplatSendEmail = @{
+  MailTo       = @('erik@knarrstudio.com')
+  MailFrom     = "$($env:computername)@mail.com"
+  msgsubj      = "Service Restarted - $(Get-Date -Format G)"
+  SmtpServers  = '192.168.0.5', '192.168.1.8'
+  MessageBody  = $EmailMessage
+  ErrorFile    = ''
+  AttachedFile = $AttachedFile
+  Verbose      = $true
+}   
+
+#region Portable_Function
+
+function Send-eMail
+{
+  [CmdletBinding(DefaultParameterSetName = 'Default')]
+  param
+  (
+    [Parameter(Mandatory,HelpMessage = 'To email address(es)', Position = 0)]
+    [String[]]$MailTo,
+    [Parameter(Mandatory,HelpMessage = 'From email address', Position = 1)]
+    [Object]$MailFrom,
+    [Parameter(Mandatory,HelpMessage = 'Email subject', Position = 2)]
+    [Object]$msgsubj,
+    [Parameter(Mandatory,HelpMessage = 'SMTP Server(s)', Position = 3)]
+    [String[]]$SmtpServers,
+    [Parameter(Position = 4)]
+    [AllowNull()]
+    $MessageBody,
+    [Parameter(Position = 5)]
+    [AllowNull()]
+    [Object]$AttachedFile,
+    [Parameter(Position = 6)]
+    [AllowEmptyString()]
+    [string]$ErrorFile = ''
+  )
+
+  $DateTime = Get-Date -Format s
+
+  if([string]::IsNullOrEmpty($MessageBody))
+  {
+    $MessageBody = ('{1} - Email generated from {0}' -f $env:computername, $DateTime)
+    Write-Warning -Message 'Setting Message Body to default message'
+  }
+  elseif(($MessageBody -match '.txt') -or ($MessageBody -match '.htm'))
+  {
+    if(Test-Path $MessageBody)
+    {
+      [String]$MessageBody = Get-Content -Path $MessageBody
+    }
+  }
+  elseif(-not ($MessageBody -is [String]))
+  {
+    $MessageBody = ('{0} - Original message was not sent as a String.' -f $DateTime)
+  }
+  else
+  {
+    $MessageBody = ("{0}`n{1}" -f $MessageBody, $DateTime)
+  }
+    
+  if([string]::IsNullOrEmpty($ErrorFile))
+  {
+    $ErrorFile = New-TemporaryFile
+    Write-Warning  -Message ('Setting Error File to: {0}' -f $ErrorFile)
+  }
+ 
+  $SplatSendMessage = @{
+    From        = $MailFrom
+    To          = $MailTo
+    Subject     = $msgsubj
+    Body        = $MessageBody
+    Priority    = 'High'
+    ErrorAction = 'Stop'
+  }
+  
+  if($AttachedFile)
+  {
+    Write-Verbose -Message 'Inserting file attachment'
+    $SplatSendMessage.Attachments = $AttachedFile
+  }
+  if($MessageBody.Contains('html'))
+  {
+    Write-Verbose -Message 'Setting Message Body to HTML'
+    $SplatSendMessage.BodyAsHtml  = $true
+  }
+  
+  foreach($SMTPServer in $SmtpServers)
+  {
+    try
+    {
+      Write-Verbose -Message ('Try to send mail thru {0}' -f $SMTPServer)
+      Send-MailMessage -SmtpServer $SMTPServer  @SplatSendMessage
+      # Write-Output $SMTPServer  @SplatSendMessage
+      Write-Verbose -Message ('successful from {0}' -f $SMTPServer)
+      Write-Host -Object ("`nsuccessful from {0}" -f $SMTPServer) -ForegroundColor green
+      Break 
+    } 
+    catch 
+    {
+      $ErrorMessage  = $_.exception.message
+      Write-Verbose -Message ("Error Message: `n{0}" -f $ErrorMessage)
+      ('Unable to send message thru {0} server' -f $SMTPServer) | Out-File -FilePath $ErrorFile -Append
+      ('- {0}' -f $ErrorMessage) | Out-File -FilePath $ErrorFile -Append
+      Write-Verbose -Message ('Errors written to: {0}' -f $ErrorFile)
+    }
+  }
+}
+
+#endregion Portable_Function
+
+Send-eMail @SplatSendEmail 
+
+#endregion Portable_Section
