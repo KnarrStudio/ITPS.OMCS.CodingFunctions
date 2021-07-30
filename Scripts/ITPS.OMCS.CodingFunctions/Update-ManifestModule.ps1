f8e432d98729b4279c4b33cdbba8226357f84f2e
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-30 09:22:53 -0500

    Update Update-ManifestModule.ps1

--- Scripts/ITPS.OMCS.CodingFunctions/Update-ManifestModule.ps1
+++ Scripts/ITPS.OMCS.CodingFunctions/Update-ManifestModule.ps1
@@ -2,19 +2,19 @@
 #requires -Version 2.0 -Modules Microsoft.PowerShell.Utility
 $SplatSettings = @{
 Path = 'D:\GitHub\KnarrStudio\ITPS.OMCS.Modules\Scripts\ITPS.OMCS.CodingFunctions\ITPS.OMCS.CodingFunctions.psd1'
+RootModule = '.\ITPS.OMCS.CodingFunctions.psm1'
 Guid = "$(New-Guid)"
 Author = 'Erik' 
 CompanyName = 'KnarrStudio'
-ModuleVersion = '1.1.0.8'
+ModuleVersion = '1.1.2.9'
 Description = 'A few functions that I use often to code with' 
 PowerShellVersion = '4.0'
 FunctionsToExport = @( 'Send-eMail', 'Get-Versions', 'Get-CurrentLineNumber', 'Set-SafetySwitch', 'Compare-FileHash', 'Import-FileData  ', 'New-TimestampFile ', 'Get-TimeStamp'
 )
 CmdletsToExport = '*'
 ModuleList = '.\ITPS.OMCS.CodingFunctions.psm1'
-
+ReleaseNotes = 'Edited the New-Timestampfile which was not handling the case when the file did not exist.  Added Validatescript to ensure filename was formatted with a "."'
 }
 
-New-ModuleManifest @SplatSettings -ReleaseNotes 'Some small edits' -RootModule '.\ITPS.OMCS.CodingFunctions.psm1'
+New-ModuleManifest @SplatSettings
 
-#FunctionsToExport = 'Send-eMail,Import-FileData,Compare-FileHash,Set-SafetySwitch,Get-Versions'
