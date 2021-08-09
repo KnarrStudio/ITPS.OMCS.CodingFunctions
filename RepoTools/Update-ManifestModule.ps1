bd28e0ad7c2e6847d77dc5e69efed872cd8708d3
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-08-09 16:21:44 -0500

    Update Update-ManifestModule.ps1

--- RepoTools/Update-ManifestModule.ps1
+++ RepoTools/Update-ManifestModule.ps1
@@ -4,9 +4,9 @@
 Write-Host $((get-item (Get-Location).Path).Parent.Name)
 
 $Major = 1     # Changes that cause the code to operate differently or large rewrites
-$minor = 1    # When an individual module or function is added
+$minor = 2    # When an individual module or function is added
 $Patch = 3     # Small updates to a function or module.  Note: This goes to zero when minor is updated
-$Manifest = 14  # For each manifest module update
+$Manifest = 15  # For each manifest module update
 
 
 $SplatSettings = @{
@@ -19,12 +19,14 @@ ModuleVersion = '{0}.{1}.{2}.{3}' -f $Major,$minor,$Patch,$Manifest
 #ModuleVersion = '1.2.0.10'
 Description = 'A few functions that I use often to code with' 
 PowerShellVersion = '4.0'
-NestedModules = @('.\Modules\ITPS.OMCS.CodingFunctions.psm1')
-FunctionsToExport = @( 'Send-eMail', 'Get-Versions', 'Get-CurrentLineNumber', 'Set-SafetySwitch', 'Compare-FileHash', 'Import-FileData  ', 'New-TimestampFile ', 'Get-TimeStamp'
-)
+NestedModules = @('.\Modules\ITPS.OMCS.CodingFunctions.psm1','.\Modules\ITPS.OMCS.MenuFunctions.psm1')
+FunctionsToExport = 'Send-eMail', 'Get-Versions', 'Get-CurrentLineNumber', 
+               'Set-SafetySwitch', 'Compare-FileHash', 'Import-FileData  ', 
+               'New-TimestampFile ', 'Get-TimeStamp', 'Show-MenuFunctions'
+               
 CmdletsToExport = '*'
 #ModuleList = '.\Modules\ITPS.OMCS.CodingFunctions.psm1'
-ReleaseNotes = 'Changed the name of the Github Repo.  Moved files into current folder structure.'
+ReleaseNotes = 'Added the Menu Examples. "Show-MenuFunctions"'
 }
 
 New-ModuleManifest @SplatSettings
