59b2291c51408a7ba601afe7f32c65e3656a7ae8
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-12-19 09:19:28 -0600

    Update Set-SafetySwitch.ps1
    script was running twice due to switch being set by default.  I wanted to keep the default behaver, but needed to be able to manually set it.  Added an "if" statement to solve issue.

--- Scripts/Set-SafetySwitch.ps1
+++ Scripts/Set-SafetySwitch.ps1
@@ -4,24 +4,23 @@
       .SYNOPSIS
       Turns on "WhatIf" for the entire script.
       Like a gun safety, "ON" will prevent the script from running and "OFF" will allow the script to make changes.
-
+      
       .PARAMETER RunScript
       Manually sets the "Safety" On/Off.
-
+      
       .PARAMETER Toggle
       changes the setting from its current state
-
+      
       .PARAMTER Bombastic
       Another word for verbose
       It also is a toggle, so that you can add it to a menu.
-
+      
       .EXAMPLE
       Set-SafetySwitch 
       Sets the WhatIfPreference to the opposite of its current setting
-
+      
       .NOTES
       Best to just copy this into your script and call it how ever you want.I use a menu
-
   #>
   
   [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'Low',DefaultParameterSetName = 'Default')]
@@ -36,48 +35,55 @@
     [Switch]$Toggle = $true
   )
 
+  
+  $Script:Bombastic = $Bombastic
   $Message = @{
     BombasticOff = 'Safety is OFF - Script is active and will make changes'
     BombasticOn  = 'Safety is ON - Script is TESTING MODE'
   }
-
   function Set-WhatIfOn
   {
-    <#.SYNOPSIS;Sets Whatif to True#>
+    <#.SYNOPSIS;Sets Whatif to True so the script will not make changes#>
     $Script:WhatIfPreference = $true
+    if ($Bombastic)
+    {
+      Write-Host -Object $($Message.BombasticOn) -ForegroundColor Green
+    }
   }
   function Set-WhatIfOff
   {
-    <#.SYNOPSIS;Sets Whatif to False#>
+    <#.SYNOPSIS;Sets Whatif to False so the script will make changes#>
     $Script:WhatIfPreference = $false
+    if ($Bombastic)
+    {
+      Write-Host -Object $($Message.BombasticOff) -ForegroundColor Red
+    }
   }
 
-  if($Toggle)
+
+  if($RunScript)
   {
-    If ($WhatIfPreference -eq $true)
+    if($RunScript -eq 'Yes')
     {
       Set-WhatIfOff
-      if ($Bombastic)
-      {
-        Write-Host -Object $($Message.BombasticOff) -ForegroundColor Red
-      }
     }
-    else
+    elseif($RunScript -eq 'No')
     {
       Set-WhatIfOn
-      if ($Bombastic)
-      {
-        Write-Host -Object $($Message.BombasticOn) -ForegroundColor Green
-      }
     }
   }
-
-  if($RunScript -eq 'Yes')
+  else
   {
-    Set-WhatIfOff
-  }
-  elseif($RunScript -eq 'No')
-  {
-    Set-WhatIfOn
+    if($Toggle)
+    {
+      If ($WhatIfPreference -eq $true)
+      {
+        Set-WhatIfOff
+      }
+      else
+      {
+        Set-WhatIfOn
+      }
+    }
   }
 }
